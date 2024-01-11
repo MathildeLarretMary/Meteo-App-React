c0d78ff0079a2e85ed98dd9b6915799b2eca7e9e
unknown <mathilde.l.art@gmail.com>
2024-01-11 09:04:46 +0100

    create visual Weather App static

---
+++ src/Weather/Weather.jsx
@@ -0,0 +1,13 @@
+import './Weather.css'
+
+export default function Weather() {
+
+    return (
+        <div className="Weather">
+            <h1>Ville</h1>
+            <h3>Pays</h3>
+            <p>5<span>°</span></p>
+            <img src='/image/cloud.jpg' alt='image'></img>
+        </div>
+    )
+}
